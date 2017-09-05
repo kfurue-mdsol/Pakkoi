49a2fdbeee3c52daecd4abeca63588514abba3d0
kfurue-mdsol <kfurue@mdsol.com>
2017-09-05 14:38:27 +0900

    initial

---
+++ main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pakkoi
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Pakkoi!");
+        }
+    }
+}
